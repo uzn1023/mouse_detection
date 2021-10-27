0fdb56ed6486d2d0e7732c3b274651c526237032
uzn1023 <e5305@gm.kochi-ct.jp>
2021-10-27 13:01:45 +0900

    movie -> grapth (Can change bout,thr but still CUI )

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 ##!/usr/bin/env python
 import os
-import tkinter
+import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+import csvproc
 import movieproc
 
 programname = "MouseDitection"
-root = tkinter.Tk()
+root = tk.Tk()
 root.withdraw()
 fTyp = [("","*.mp4")]
 tkinter.messagebox.showinfo(programname,'Please select video file.')
@@ -16,7 +20,10 @@ tkinter.messagebox.showinfo(programname,'Please select output folder.')
 outdir = tkinter.filedialog.askdirectory(initialdir = "..")
 
 csv_out = movieproc.proc(movie,outdir)    # mouse ditection and calclate moving -> csv file
-print(csv_out)
 
-
-
+while(1):
+    Threshold = float(input('Enter Threshould: '))
+    bout = float(input('Enter bout: '))
+    plt.close()
+    fig = csvproc.proc(csv_out,Threshold,bout)
+    plt.pause(0.01)
