696a7624ab8c52b772045ff259248eda6bff3548
uzn1023 <e5305@gm.kochi-ct.jp>
2021-11-04 10:24:40 +0900

    Made movei player

--- src/main.py
+++ src/main.py
@@ -5,53 +5,78 @@ import tkinter.filedialog
 import tkinter.messagebox
 from tkinter import ttk
 
+import cv2
 import matplotlib.pyplot as plt
+import PySimpleGUI as sg
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import csvproc
 import movieproc
 
+
+def runmovie(moviename):
+    vidFile = cv2.VideoCapture(moviename)
+    num_frames = vidFile.get(cv2.CAP_PROP_FRAME_COUNT)
+    fps = vidFile.get(cv2.CAP_PROP_FPS)
+
+    sg.theme('Black')
+
+    layout = [    [sg.Image(filename='', key='-image-')],
+                  [sg.Slider(range=(0, num_frames),size=(50, 10), orientation='h', key='-slider-')],
+                  [sg.Button('Exit', size=(7, 1), font='Helvetica 14')]]
+
+    window = sg.Window('　動画ファイルを再生するアプリ　', layout, no_titlebar=False, location=(0, 0), resizable=True)
+
+    image_elem = window['-image-']
+    slider_elem = window['-slider-']
+
+    cur_frame = 0
+    while vidFile.isOpened():
+        # イベントを取得
+        event, values = window.read(timeout=0)
+
+        # 「Exit」ボタン押下時の処理
+        if event in ('Exit', None):
+            break
+
+        #　ビデオファイルからの読み込み
+        ret, frame = vidFile.read()
+
+        #　データが不足している場合は、ループを停止させます。
+        if not ret:  # if out of data stop looping
+            break
+
+        #　スライダーを手動で動かした場合は、指定したフレームにジャンプします
+        if int(values['-slider-']) != cur_frame-1:
+            cur_frame = int(values['-slider-'])
+            vidFile.set(cv2.CAP_PROP_POS_FRAMES, cur_frame)
+
+        #　スライダー表示を更新
+        slider_elem.update(cur_frame)
+        cur_frame += 1
+
+        #　カメラ映像を圧縮して、画像表示画面'-image-'を更新する
+        frame = cv2.resize(frame,(int(frame.shape[1] / 3),int(frame.shape[0] / 3)))
+        secs = cur_frame // 30
+        sec = secs % 60
+        mins = secs // 60
+        min = mins % 60
+        hr = mins //60
+
+        timestump = str(hr) + ":" + str(min).zfill(2) + ":" + str(sec).zfill(2)
+        cv2.putText(frame, timestump, org=(30,30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.0,color=(0,0,0),thickness=2, lineType=cv2.LINE_4)
+        imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+        image_elem.update(data=imgbytes)
+
 programname = "MouseDitection"
 root = tk.Tk()
-root.title('Input parameters')
-root.geometry("300x200")
-
+root.withdraw()
 fTyp = [("","*.mp4")]
 tkinter.messagebox.showinfo(programname,'Please select video file.')
 movie = tkinter.filedialog.askopenfilename(filetypes = fTyp, initialdir = "..")
 tkinter.messagebox.showinfo(programname,'Please select output folder.')
 outdir = tkinter.filedialog.askdirectory(initialdir = "..")
 
+runmovie(movie)
+
 csv_out = movieproc.proc(movie,outdir)    # mouse ditection and calclate moving -> csv file
-
-
-label1 = ttk.Label(root, text='Threshold')
-label1.grid(row=1,column=1)
-label1.grid_configure(padx=10, pady=15)
-
-var = tk.IntVar(root)
-var.set(500)
-sp1 = tk.Spinbox(root,textvariable=var,from_=0,to=100000,increment=100)
-sp1.grid(row=1,column=2)
-sp1.grid_configure(padx=10, pady=15)
-
-label2 = ttk.Label(root, text='Bout')
-label2.grid(row=2,column=1)
-label2.grid_configure(padx=10, pady=15)
-
-var2 = tk.DoubleVar()
-var2.set(0.25)
-ary=[0, 0.25, 0.5, 1, 2]
-sp2 = tk.Spinbox(root,textvariable=var2,value=ary)
-sp2.grid(row=2,column=2)
-sp2.grid_configure(padx=10, pady=15)
-
-frame = ttk.Frame(root)
-frame.grid(row=3,column=2)
-frame.grid_configure(padx=10, pady=15)
-fig = plt.figure()
-button = tk.Button(frame, text="Calculate", command=lambda:csvproc.proc(csv_out,var.get(),var2.get(),fig))
-#button = tk.Button(frame, text="Calculate", command=lambda:print(var2.get()))
-button.grid()
-
-root.mainloop()
